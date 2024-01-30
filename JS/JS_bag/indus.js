f81dc31cab68dccf4da2e9938cb0defa6cac7769
il-shn <iliashnurenko@gmail.com>
2024-01-30 13:50:18 +0200

    JS

--- JS/JS_bag/indus.js
+++ JS/JS_bag/indus.js
@@ -1,9 +1,10 @@
 let userNickname = "redTime";
 let userScore = 2300;
-// console.log(userNickname, userScore)
+// console.log(userNickname, userScore);
 let price = prompt("Вкажіть ціну покупки", "Пусте значення");
 let quantity = prompt("Вкажіть кількість продуктів", "Пусте значення");
 let sum = price*quantity;
 console.log('The price is', sum, 'UAH');
+// 5% від попередньої суми 
 let discount = (5*sum)/100;
 console.log('your discount', discount);
