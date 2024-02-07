d3545fed2818e239f09c7d623562b72dc06c7ee6
il-shn <iliashnurenko@gmail.com>
2024-02-07 17:22:35 +0200

    loopsHW

---
+++ JS/js_03/js-pack/loops.js
@@ -0,0 +1,47 @@
+// let a = "";
+// let b = "";
+// let c = "";
+// let result = Number(prompt(`Це калькулятор ${a+c+b}`))
+// if(c=="+"){
+//     alert(a+b)
+// }
+// else if(Number(prompt(a-b))){
+//     alert(a-b)
+// }
+// else if(Number(prompt(a*b))){
+//     alert(a*b)
+// }
+// else if(Number(prompt(a/b))){
+//     alert(a/b)
+// }
+// else{
+//     console.log("error");
+// }
+// TRAAAASH!!!
+
+
+// // за допомогою циклу вивести в консоль всі парні значення числа 20
+
+// let num = 2;
+// do{
+//     console.log(num);
+//     num+=2;
+// }while(num<=20);
+
+// // зробити цикл який виводить дані навпаки від більшого до меншогo.
+
+// let num =100;
+// while(num>=5) {
+//     console.log(num);
+//     num-=5;
+// };
+
+// for(let i=100;i>0;i-=5){
+//     console.log(`Loop value ${i}`)
+// }
+
+// // реалізуйте таблицю множення за допомогою циклу для конкретного числа (наприклад, для числа 5) за допомогою циклу for:
+
+// for(let i=1; i<=10; i++){
+//     console.log(`${i} * 5 = ${i*5}`)
+// }
