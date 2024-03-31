54e9308690c0e6ec6d19b03f8c40e710facc8cb3
il-shn <iliashnurenko@gmail.com>
2024-03-31 09:47:15 +0300

    HW13

--- JS/js_13/regex.js
+++ JS/js_13/regex.js
@@ -19,11 +19,52 @@ console.log(result2);
 // ============= 3 Завдання =================
 // Створити регулярний вираз, який видаляє всі HTML теги з рядка. Наприклад, після обробки рядка <p>Це - простий <b>текст</b>.</p> має залишитися Це - простий текст.
 
-let theText = `4{<p>}Це - простий {<b>}текст{</b>}.{</p>}`
+let theText = '<p>Це - простий <b>текст</b>.</p>'
+
+let searchTag = /<\/?\w+>/g;
+let delTag = theText.replace(searchTag, '')
+console.log(delTag);
 
 // ============= 4 Завдання =================
 // Розробити регулярний вираз для перевірки сили пароля. Пароль повинен містити принаймні одну велику літеру, одну маленьку літеру, одну цифру, один спеціальний символ (!@#$%^&*) і мати довжину від 8 до 20 символів.
 
+// let checkPassword = /[a-zA-Z0-9!@#\$%\^&\*]{8,20}/g;
+let checkPassword = /^(?=.*[a-z])(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,20}$/;
+let passwordSample = 'wWeE103#';
+let passwordSample2 = 'HKky465y7^%$@87gerd';
+console.log(checkPassword.test(passwordSample));
+console.log(checkPassword.test(passwordSample2));
+
 // ============= 5 Завдання =================
 // Створіть групу радіокнопок для оцінки від 1 до 5.
 // Додайте обробник події onchange для виявлення вибору користувача, а потім виведіть відповідне повідомлення (наприклад, "Дякуємо за вашу оцінку!").
+
+window.onload = ()=>{
+    let [...form] = document.querySelector('form')
+    // function rating() {
+    //     for (const inp of inputAr) {
+    //         if (inp.checked) {
+    //             inp.value===rate
+    //             console.log(rate);
+    //             alert("Дякуємо за вашу оцінку!")
+    //         }
+            // inp.find(inp.checked=>inp.value===rate)
+            // inp.onclick = function(){
+    //     }
+    // }
+    for (const inp of form) {
+        inp.onchange = function(){
+            if (inp.checked) {
+                console.log(inp.value);        
+                alert(`Дякуємо за вашу оцінку ${inp.value}`);    
+            } 
+        }    
+    }
+    // select2.addEventListener('change', function(){
+    //     let selectValue = this.value;
+    //     let info2 = countryInfo.find(country=>country.name === selectValue)
+    //     let infoBlock = document.querySelector('.infoBlock')
+    //     infoBlock.innerHTML = `Capital: ${info2.capital}, population: ${info2.population}`;
+    // });
+
+}
