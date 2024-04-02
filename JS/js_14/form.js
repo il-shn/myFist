ba32d679d6bd03399e87119e53c0403abff37082
il-shn <iliashnurenko@gmail.com>
2024-04-02 14:35:27 +0300

    hw_14

---
+++ JS/js_14/form.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+    let btn = document.querySelector('button')
+    let form = document.querySelector('form')
+    let checkUsername = /[a-zA-Z]/g;
+    let checkEmail = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})*(\.[a-z]{2,8})?$/;
+    let checkNumber = /\+*[0-9]{10,12}/g;
+    let checkPassword = /^(?=.*[a-z])(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{10,30}$/g;
+    let inputUserName = document.querySelector('#inputUsername')
+    let inputEmail = document.querySelector('#inputEmail')
+    let inputPhone = document.querySelector('#inputPhoneNumber')
+    let inputPassword1 = document.querySelector('#inputPassword1')
+    let inputPassword2 = document.querySelector('#inputPassword2')
+
+    form.onchange = function(){
+        if (checkUsername.test(inputUserName.value)==true && checkEmail.test(inputEmail.value)==true && checkNumber.test(inputPhone.value)==true && checkPassword.test(inputPassword1.value)==true) {
+            if (inputPassword1=inputPassword2) {
+                console.log('Кнопку розблоковано!');
+                btn.disabled = false;
+                btn.style.color = 'white';
+                btn.style.backgroundColor = 'rgb(41, 41, 255';
+                btn.addEventListener('mouseenter', ()=>
+                    btn.style.backgroundColor = 'rgb(29, 29, 183)')
+                btn.addEventListener('mouseleave', ()=>
+                    btn.style.backgroundColor = 'rgb(41, 41, 255')
+            }else{
+                console.log('не підходить пароль');
+            }
+        } else {
+            console.log('різні паролі!');
+        }
+    }  
+    // let contentWrap = document.querySelector('.content-wrapper')
+    // btn.onclick = function(){
+    //    let loaderBlock = document.createElement('div')
+    //    contentWrap.appendChild(loaderBlock)
+    //    loaderBlock.style.height = '200px'
+    //    loaderBlock.style.width = '200px'
+    //    loaderBlock.style.backgroundColor = 'white'
+    //    loaderBlock.style.backgroundColor = 'white'
+    // }   
+}   
